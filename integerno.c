e335cd4ffb24a536080bee8e6db510a4641af3c0
Manoj Goud <151609313+GuduguntlaManoj@users.noreply.github.com>
2026-02-06 22:14:58 +0530

    Add files via upload

---
+++ integerno.c
@@ -0,0 +1,11 @@
+#include <stdio.h>    
+    void main()
+    {
+      int num;
+     
+      printf("Enter a number\n");
+      scanf("%d", &num); // %d is used for an integer
+     
+      printf("The number is: %d\n", num);
+     
+    }
